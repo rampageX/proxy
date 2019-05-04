5d10c4bc3a57a5ad43218b583dd3bba4789423fa
Punk <punkdm@gmail.com>
2019-05-04 16:26:51 +0800

    Update proxy.go

--- proxy.go
+++ proxy.go
@@ -104,8 +104,9 @@ func (p Proxy) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (
 
 				// Check if the reply is correct; if not return FormErr.
 				if !state.Match(reply) {
-//					formerr := state.ErrorMessage(dns.RcodeFormatError)
-//					w.WriteMsg(formerr)
+                    formerr := new(dns.Msg)
+                    formerr.SetRcode(state.Req, dns.RcodeFormatError)
+					w.WriteMsg(formerr)
 					return 0, taperr
 				}
 
